a08258422cae4a0d304005fb555e9d03a7c558e9
aaa066 <asifabdullah6@gmail.com>
2020-01-28 04:18:39 +0600

    Initial commit

---
+++ app/src/main/java/com/example/resurrected_app/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.resurrected_app;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button click ;
+    public static TextView data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        click = (Button) findViewById(R.id.btnId);
+        data = (TextView) findViewById(R.id.fetchedData);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchData process = new fetchData();
+                process.execute();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/resurrected_app/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.resurrected_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    // private final progressBar progressBar = new ThreadLocal<com.example.fristapp.progressBar>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+
+        }, 3000);
+
+    }
+}
